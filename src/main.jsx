923fa1c72cb227eaed49bfc16d5a9c8abb5e454b
Abdelrahman Reyad <92989840+Abdelrahman-7z7@users.noreply.github.com>
2025-03-26 11:50:27 +0300

    initializing react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {Router, RouterProvider, createBrowserRouter} from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
+// the createBrowserRouter takes array as an argument => an array of all the routers you want to have => and that list is an OBJECT
+
+const router = createBrowserRouter([
+  {path:'/', element: <App />}, //<our-domain> entering our domain => we should also define the element that should be run when we call that path 
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* RouterProvider => watch the URL and start rendering different components, it should be run instead of <App/> */}
+    {/* <App /> */}
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
