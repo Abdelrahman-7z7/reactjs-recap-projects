447fe1dbe5d68cb5e39f96584cf34b52225aaf30
Abdelrahman Reyad <92989840+Abdelrahman-7z7@users.noreply.github.com>
2025-03-26 12:18:50 +0300

    adding the nested-route using RootLayout and outlet from react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {Router, RouterProvider, createBrowserRouter} from 'react-router-dom'
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+import RootLayout from './Routes/RootLayout.jsx'
+
 import './index.css'
 import App from './App.jsx'
+import NewPost from './components/NewPost.jsx'
 
 // the createBrowserRouter takes array as an argument => an array of all the routers you want to have => and that list is an OBJECT
 
 const router = createBrowserRouter([
-  {path:'/', element: <App />}, //<our-domain> entering our domain => we should also define the element that should be run when we call that path 
+  //we use RootLayout for handling nested-routes
+  {
+    path:'/', 
+    element:<RootLayout /> , 
+    children:[
+      {path:'/', element: <App />}, //<our-domain> entering our domain => we should also define the element that should be run when we call that path 
+      {path: '/create-post', element: <NewPost />}
+  ]}
 ]);
 
 createRoot(document.getElementById('root')).render(
